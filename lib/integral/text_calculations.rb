3bfcb7de5254e9861157554aee64a42b95fd9cf8
Sergey Pedan <sergey.pedan@gmail.com>
2019-09-01 19:40:13 +0300

    Adds annotaiton

--- lib/integral/text_calculations.rb
+++ lib/integral/text_calculations.rb
@@ -2,11 +2,14 @@
 
 module Integral
   module TextCalculations
+
+  	# need to replace with Integral::TextHelepr
     def self.text_to_a4(text)
       fail ArgumentError unless text.nil? || text.is_a?(String)
       str = text.to_s
       return 0 if str == ""
       (str.size / 1800.0).round(1)
     end
+
   end
 end
